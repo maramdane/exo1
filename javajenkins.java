4d919a5d4f317d1601fe97e0b27a99e658272eeb
maramdane <aramdane@thenuumfactory.fr>
2023-07-20 14:38:31 +0200

    edit World Devops 2

--- javajenkins.java
+++ javajenkins.java
@@ -1,5 +1,5 @@
 public class javajenkins {
     public static void main(String[] args) {
-        System.out.println("Bonjour, World Devops !");
+        System.out.println("Bonjour, World Devops 2!");
     }
 }
