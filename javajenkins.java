49e64c25eca7ea94b4bddb3376af5339aed5dee9
maramdane <aramdane@thenuumfactory.fr>
2023-07-20 14:52:48 +0200

    edit 3

--- javajenkins.java
+++ javajenkins.java
@@ -1,5 +1,5 @@
 public class javajenkins {
     public static void main(String[] args) {
-        System.out.println("Bonjour, World Devops 2!");
+        System.out.println("Bonjour, World Devops 3!");
     }
 }
